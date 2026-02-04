280b26b76326d3f9279e9db2dc792b15bac87aaf
Akansha Roy <akansharoy2008@gmail.com>
2026-02-04 23:13:20 +0530

    initial commit

---
+++ src/main/java/com/akansha/peak_server/PeakServerApplication.java
@@ -0,0 +1,13 @@
+package com.akansha.peak_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akansha/peak_server/PeakServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akansha.peak_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
